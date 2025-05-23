a13e22e9300c58d9b69268617752f556271e7769
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-05-23 15:10:47 +0530

    test : trying something

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import time
 # from streamlit_chat import message as chat
 from streamlit.components.v1 import html
 
-# html("""
-# <script>
-# window.top.document.querySelectorAll('[href*="streamlit.io"]').forEach(e => e.setAttribute("style", "display: none;"));
-# </script>
-# """)
+html("""
+<script>
+window.top.document.querySelectorAll('[href*="streamlit.io"]').forEach(e => e.setAttribute("style", "display: none;"));
+</script>
+""")
 
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
